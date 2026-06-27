e5f631c4006810b560e80374dfcac8d58e377722
isha661 <ishaluhun@gmail.com>
2026-06-27 13:53:22 +0545

    multiple input field

--- my-react-app/src/inputField.jsx
+++ my-react-app/src/inputField.jsx
@@ -21,24 +21,51 @@ import { useState } from "react";
 
 
 /// Age input 
-function State (){
-     const[age, setAge] = useState("");
+// function State (){
+//      const[age, setAge] = useState("");
      
-     function myAge(event){
+//      function myAge(event){
 
-        setAge(event.target.value);
-     }
+//         setAge(event.target.value);
+//      }
 
 
 
-     return (
-        <div>
-            <input type="number" onChange={myAge}/>
+//      return (
+//         <div>
+//             <input type="number" onChange={myAge}/>
 
-            <h1> My age is {age}</h1>
-        </div>
+//             <h1> My age is {age}</h1>
+//         </div>
 
        
-     );
+//      );
+// }
+// export default State;
+
+
+
+
+// Multiple input
+function State(){
+    const [name ,setName] = useState("");
+    const [age, setAge] = useState("");
+
+    function handelAge(event){
+        setAge(event.target.value);
+    }
+
+    function handelName(event){
+        setName(event.target.value);
+    }
+
+    return(
+
+        <div>
+            <input type="text" placeholder="name" onChange={handelName}/>
+            <br></br>
+            <input type="number" placeholder="age" onChange={handelAge}/>
+        </div>
+    );
 }
 export default State;
