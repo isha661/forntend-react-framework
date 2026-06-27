76fc930b8444e6e487928394e7de0aa31bb75a1c
isha661 <ishaluhun@gmail.com>
2026-06-27 13:39:57 +0545

    age input field

--- my-react-app/src/inputField.jsx
+++ my-react-app/src/inputField.jsx
@@ -1,20 +1,44 @@
 import { useState } from "react";
 
-function State(){
+// function State(){
 
-    const [name, setName] = useState("");
+//     const [name, setName] = useState("");
 
-    function handleChange(event){
-        setName(event.target.value);
-    }
+//     function handleChange(event){
+//         setName(event.target.value);
+//     }
 
-    return(
+//     return(
 
-        <div>
-            <input type="text" onChange={handleChange}/>
+//         <div>
+//             <input type="text" onChange={handleChange}/>
             
-            <h1> Hello {name}</h1>
+//             <h1> Hello {name}</h1>
+//         </div>
+//     );
+// }
+// export default State;
+
+
+/// Age input 
+function State (){
+     const[age, setAge] = useState("");
+     
+     function myAge(event){
+
+        setAge(event.target.value);
+     }
+
+
+
+     return (
+        <div>
+            <input type="number" onChange={myAge}/>
+
+            <h1> My age is {age}</h1>
         </div>
-    );
+
+       
+     );
 }
 export default State;
