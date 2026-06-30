34d569be86a06e81eee7f1c72e82a84228e4e1b4
isha661 <ishaluhun@gmail.com>
2026-06-30 20:06:38 +0545

    Name form using usestate,onSubmit

--- my-react-app/src/form.jsx
+++ my-react-app/src/form.jsx
@@ -1,17 +1,56 @@
+// learning form and onSubmit
+
+// function Forms(){
+
+//     function handleForm(event){
+//         event.preventDefault();  //with this it wont refresh
+//         alert("form submiteed")
+//     }
+
+
+//     return(
+//         <form onSubmit={handleForm}>
+//             <button type="submit"> Submit</button>
+
+//         </form>
+//     );
+// }export default Forms;
 
 
 
-function Forms(){
 
-    function handleForm(){
-        alert("form submiteed")
+//Name Form
+import {useState} from "react";
+
+function Form(){
+
+    const[name, setName] = useState("");
+
+    function handleSubmit(event){
+        //prevent page refresh
+        event.preventDefault();
+
+        alert(`Welcome ${name}`);
     }
 
 
     return(
-        <form onSubmit={handleForm}>
-            <button type="submit"> Submit</button>
 
-        </form>
+        <form onSubmit={handleSubmit}>
+
+             {/*Input Field */}
+            <input type="text" 
+            placeholder="Enter your name" 
+            onChange={(event) => setName(event.target.value)}/>
+
+
+             {/*Submit button*/}
+            <button type="submit">
+                Submit
+            </button>
+
+        </form>    
+        
     );
-}export default Forms;
+}
+export default Form;
